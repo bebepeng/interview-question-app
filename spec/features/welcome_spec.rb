b974aec8af69c33dd9fab33575d2518df0282224
Bebe Peng <bebepeng@gmail.com>
2014-06-27 16:48:01 -0600

    [#74058294] welcome page can be viewed at root

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature 'Welcome Page' do
+  scenario 'users can see the welcome page' do
+    visit root_path
+    expect(page).to have_content 'Phone Interview'
+  end
+end
